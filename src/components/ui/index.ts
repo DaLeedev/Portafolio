49185aeb1c8984266914838c08b39a91ebbcc28a
Luis David Lemus Perea <luisdlperea@hotmail.com>
2026-01-11 20:35:38 -0500

    feat: se crea exploraciones de barril para archivos jsx en directory 'ui'

---
+++ src/components/ui/index.ts
@@ -0,0 +1,45 @@
+// Exportar todos los archivos tsx en este directorio como exporaciones de barril
+
+export * from "./accordion";
+export * from "./aspect-ratio";
+export * from "./avatar";
+export * from "./badge";
+export * from "./button";
+export * from "./card";
+export * from "./checkbox";
+export * from "./dialog";
+export * from "./dropdown-menu";
+export * from "./hover-card";
+export * from "./input";
+export * from "./label";
+export * from "./menubar";
+export * from "./navigation-menu";
+export * from "./popover";
+export * from "./progress";
+export * from "./radio-group";
+export * from "./scroll-area";
+export * from "./select";
+export * from "./separator";
+export * from "./sidebar";
+export * from "./slider";
+export * from "./sonner";
+export * from "./switch";
+export * from "./tabs";
+export * from "./textarea";
+export * from "./toggle";
+export * from "./toggle-group";
+export * from "./tooltip";
+export * from "./breadcrumb";
+export * from "./command";
+export * from "./context-menu";
+export * from "./pagination";
+export * from "./skeleton";
+export * from "./table";
+export * from "./use-mobile";
+export * from "./utils";
+export * from "./chart";
+export * from "./form";
+export * from "./calendar";
+export * from "./collapsible";
+
+
