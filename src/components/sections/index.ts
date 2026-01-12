bf25f018b896b866d8ea2872d161e8866ff2f278
Luis David Lemus Perea <luisdlperea@hotmail.com>
2026-01-11 20:39:13 -0500

    feat: se crea exploraciones de barril para archivos jsx en directory 'sections'

---
+++ src/components/sections/index.ts
@@ -0,0 +1,7 @@
+// Exportar todos archivos tsx en este directorio como exporaciones de barril
+
+export * from "./Certificados";
+export * from "./Hero";
+export * from "./Projects";
+export * from "./Skill";
+export * from "./Contact";
