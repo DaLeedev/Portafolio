97e3b09b4c6e01ef00697886b1082352f178935d
Luis David Lemus Perea <luisdlperea@hotmail.com>
2026-01-16 19:04:07 -0500

    style: mejorar tamanio y footer

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -3,7 +3,7 @@ import { Code2 } from "lucide-react";
 
 export const Footer = () => {
   return (
-    <footer className="py-8 px-4 sm:px-6 lg:px-8 border-t border-zinc-800">
+    <footer className="py-2.5 px-4 sm:px-6 lg:px-8 border-t border-zinc-800">
       <div className="max-w-7xl mx-auto">
         <div className="flex flex-col sm:flex-row justify-between items-center gap-4">
           <p className="text-zinc-500 text-sm">
